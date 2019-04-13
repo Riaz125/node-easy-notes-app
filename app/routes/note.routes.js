3dbf88f854a69ee55eabd8e6c9caff73f18eb9a4
Rez Khan <reswanul@gmail.com>
2019-04-13 03:11:48 +0000

    Removed route to find by date

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
     // Retrieve a single Note with noteId
     app.get('/notes/:noteId', notes.findOne);
 
-    app.get('/notes/date/:datestr', notes.findDate)
+    // app.get('/notes/date/:datestr', notes.findDate);
 
     // Update a Note with noteId
     app.put('/notes/:noteId', notes.update);
